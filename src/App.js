f79ec4ebc0208d284efbc35891e00935c6530c9a
Karim-Nabarawi <k.nabarawi@gmail.com>
2021-12-15 16:16:38 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { loadMovies } from "./actions/moviesAction";
+import Menu from "./Components/Menu";
 import MovieList from "./Components/MovieList";
 
 function App() {
@@ -14,12 +15,15 @@ function App() {
   const { popular, upcoming } = useSelector((state) => state.movies);
 
   return (
-    <div className="container">
-      <h3>Popular</h3>
-      <MovieList MoviesList={popular} />
-      <h3>Upcoming Releases</h3>
-      <MovieList MoviesList={upcoming} />
-    </div>
+    <>
+      <Menu />
+      <div className="container">
+        <h3>Popular</h3>
+        <MovieList MoviesList={popular} />
+        <h3>Upcoming Releases</h3>
+        <MovieList MoviesList={upcoming} />
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Menu.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a href="/" className="navbar-brand">
+          Movies
+        </a>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a href="/geners" className="nav-link">
+                Geners
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Menu;
