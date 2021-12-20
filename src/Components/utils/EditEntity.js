35e6c3b6d23a47f3b1e949166791af37f2722bde
Karim-Nabarawi <k.nabarawi@gmail.com>
2021-12-20 16:46:16 +0200

    - refactor genre edit to entity edit

---
+++ src/Components/utils/EditEntity.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import DisplayErrors from "./DisplayErrors";
+import GenericLoading from "./GenericLoading";
+
+const EditEntity = ({ url, entityName, children }) => {
+  let { id } = useParams();
+  const navigation = useNavigate();
+  const [entity, setEntity] = useState();
+  const [errors, setErrors] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${url}/${id}`).then((res) => {
+      console.log(res);
+      setEntity(res.data.name);
+    });
+  }, [url, id]);
+
+  const edit = async (entityToEdit) => {
+    try {
+      await axios.put(`${url}/${id}`, entityToEdit);
+      navigation("/genres");
+    } catch (error) {
+      if (error && error.response) setErrors(error.response.data);
+    }
+  };
+
+  return (
+    <div className="rr">
+      <h3>Edit {entityName}</h3>
+      <DisplayErrors errors={errors} />
+      <GenericLoading data={entity}>{children(entity, edit)}</GenericLoading>
+    </div>
+  );
+};
+export default EditEntity;
--- src/pages/Genres/GenreEdit.js
+++ src/pages/Genres/GenreEdit.js
@@ -1,46 +1,21 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import { genresURL } from "../../Api";
 import GenreForm from "../../Components/forms/GenreForm";
-import GenericLoading from "../../Components/utils/GenericLoading";
-import DisplayErrors from "../../Components/utils/DisplayErrors";
+import EditEntity from "../../Components/utils/EditEntity";
 
 const GenreEdit = () => {
-  let { id } = useParams();
-  const navigation = useNavigate();
-  const [genre, setGenre] = useState();
-  const [errors, setErrors] = useState([]);
-
-  useEffect(() => {
-    axios.get(`${genresURL()}/${id}`).then((res) => {
-      console.log(res);
-      setGenre(res.data.name);
-    });
-  }, [id]);
-
-  const edit = async (genreToEdit) => {
-    try {
-      await axios.put(`${genresURL()}/${id}`, genreToEdit);
-      navigation("/genres");
-    } catch (error) {
-      if (error && error.response) setErrors(error.response.data);
-    }
-  };
-
   return (
     <div className="rr">
-      <h3>Edit Genre {id}</h3>
-      <DisplayErrors errors={errors} />
-      <GenericLoading data={genre}>
-        <GenreForm
-          initialValues={{ name: genre }}
-          onSubmit={async (value) => {
-            await edit(value);
-          }}
-        />
-      </GenericLoading>
+      <EditEntity url={genresURL()} entityName="Genre">
+        {(entity, edit) => (
+          <GenreForm
+            initialValues={{ name: entity }}
+            onSubmit={async (value) => {
+              await edit(value);
+            }}
+          />
+        )}
+      </EditEntity>
     </div>
   );
 };
